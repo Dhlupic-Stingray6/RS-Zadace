3b8afbb6c13ec6de90b20b2a6c8bd2aa55072adc
Dhlupic6 <hlupic.dominik20@gmail.com>
2024-11-23 23:51:06 +0100

    ispravak % modulo

--- RS-1-zadace/zadatak_8.py
+++ RS-1-zadace/zadatak_8.py
@@ -2,7 +2,7 @@ lista = [1,2,3,4,5,6,7,8,9,10]
 
 def filtriraj_parne(lista):
     for element in lista:
-        if(element /2 != 0):
+        if(element % 2 != 0):
             lista.remove(element)
         
 filtriraj_parne(lista);
