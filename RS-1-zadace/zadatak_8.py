733efa3a59cd97e997d3010c51f22452c61fc95d
Dhlupic6 <hlupic.dominik20@gmail.com>
2024-11-23 21:58:28 +0100

    commit 8.zadaka iz RS1 izvan roka

---
+++ RS-1-zadace/zadatak_8.py
@@ -0,0 +1,9 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+def filtriraj_parne(lista):
+    for element in lista:
+        if(element /2 != 0):
+            lista.remove(element)
+        
+filtriraj_parne(lista);
+print(lista)
