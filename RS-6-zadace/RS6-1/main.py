45ddc787780a3f2ccf39af712ee8f32b86847337
Dhlupic6 <hlupic.dominik20@gmail.com>
2025-01-25 13:04:11 +0100

    inicijalizacija RS6 1

---
+++ RS-6-zadace/RS6-1/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from typing import List
+from models import Film, CreateFilm
+
+app = FastAPI()
+
+filmovi = [
+{"id": 1, "naziv": "Titanic", "genre": "drama", "godina": 1997},
+{"id": 2, "naziv": "Inception", "genre": "akcija", "godina": 2010},
+{"id": 3, "naziv": "The Shawshank Redemption", "genre": "drama", "godina": 1994},
+{"id": 4, "naziv": "The Dark Knight", "genre": "akcija", "godina": 2008}
+]
+
+
+
+@app.get('/filmovi', response_model=List[Film])
+def dohvati_filmove(genre = None, min_godina : int = 2000):
+    pronadeni_filmovi = [film for film in filmovi if(genre is None) and (film['godina'] >= min_godina)]
+    return pronadeni_filmovi
+
+
+@app.get('/filmovi/{id}', response_model=Film)
+def dohvati_filmove(id: int):
+    pronaden_film = next((film for film in filmovi if film['id'] == id), None)
+    return pronaden_film
+
+
+@app.post('/filmovi', response_model=Film)
+def add_film(film: CreateFilm):
+    new_id = len(filmovi) + 1
+    film_s_id : Film = {"id": new_id, **film.model_dump()}
+    filmovi.append(film_s_id)
+    return film_s_id
---
+++ RS-6-zadace/RS6-1/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class Film(BaseModel):
+    id: int
+    naziv: str
+    genre: str
+    godina: int
+
+
+class CreateFilm(BaseModel):
+    naziv: str
+    genre: str
+    godina: int
